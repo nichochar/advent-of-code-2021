9398433560f99fc0ed3feaf8ed8e612fd1f27731
Nicholas Charriere <nicholas.charriere@getcruise.com>
2021-12-01 19:53:13 -0800

    Day 1 first star and inputs dir

--- day1.py
+++ day1.py
@@ -2,6 +2,30 @@
 import os
 
 
+def read(relative_path):
+    with open(relative_path, 'r') as f:
+        data = f.read()
+        lines = data.strip().split('\n')
+
+    lines = [int(elt) for elt in lines if elt]
+    return lines
+
+
+def solve(lines):
+    count = 0
+    for i in range(len(lines) - 1):
+        current = lines[i]
+        next_ = lines[i + 1]
+        if current < next_:
+            count += 1
+    print(current, next_)
+    return count
+
+
 if __name__ == '__main__':
     name = os.path.basename(__file__).split('.py')[0]
     print(f"Solving {name} for advent of code")
+
+    lines = read('inputs/day1.txt')
+    result = solve(lines)
+    print(f"Result: {result}")
